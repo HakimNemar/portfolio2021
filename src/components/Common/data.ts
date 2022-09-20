b2ea73a8c0c0c4df95558c0e9b9cbf72ddd8391b
hakim-nemar <hakim.nemar@epitech.eu>
2022-09-20 19:14:22 +0200

    ajout des description dans data parcours

--- src/components/Common/data.ts
+++ src/components/Common/data.ts
@@ -1,11 +1,19 @@
 export const DataParcours = [
     {
         title: "Intégrateur Développeur Web",
+        entreprise: "Adelios",
+        contrat: "CDD",
+        date: "2021 / 2022",
+        logo: "adelios.png",
+        description: "Renfort sur la maintenance des sites ainsi que le développement de Tolefi et SecondLife en Wordpress (utilisant Github), animation faite en Javascript et Sass en utilisant gulp, création des environnements de développement et de preprod"
+    },
+    {
+        title: "Intégrateur Développeur Web",
         entreprise: "Groupe Duval",
         contrat: "WebMaster CDI",
-        // ecole: "Samsung CAMPUS",
         date: "2021 / 2022",
-        logo: "groupeduval.png"
+        logo: "groupeduval.png",
+        description: "Développement et création d’un git du site Groupe Duval en wordpress en utilisant Sass et TypeScript ainsi qu’une newsletter interne et maintenance des sites wordpress du groupe"
     },
     {
         title: "Intégrateur Développeur Web",
@@ -13,27 +21,31 @@ export const DataParcours = [
         contrat: "Alternance",
         // ecole: "Samsung CAMPUS",
         date: "2020 / 2021",
-        logo: "adelios.png"
+        logo: "adelios.png",
+        description: "Développement des sites LaFrenchCom et Adna en Wordpress (utilisant Github), animation faite en Javascript et Sass en utilisant gulp, création des environnements de développement et de preprod ainsi que la mise en production des sites, maintenance et modification des sites et applications web de l’agence fait en TypeScript"
     },
     {
-        title: "« Web & Apps Coding » Intégrateur Développeur Web Full Stack",
+        title: "Intégrateur Développeur Web Full Stack",
         ecole: "Samsung CAMPUS by {Epitech.}",
         date: "2019 / 2021",
-        logo: "campus.png"
+        logo: "campus.png",
+        description: "Pédagogie d’apprentissage autonome par projet (individuel et en groupe)"
     },
     {
         title: "Chauffeur Livreur",
         entreprise: "2M Flash Transport",
         contrat: "CDI",
         date: "2018 / 2019",
-        logo: "2m.jpg"
+        logo: "2m.jpg",
+        description: "Livraison de produits pharmaceutiques"
     },
     {
         title: "Agent de Sûreté Aéroportuaire",
         entreprise: "ICTS",
         contrat: "CDD",
         date: "2017",
-        logo: "icts.png"
+        logo: "icts.png",
+        description: "Contrôler l'accès des passagers à l'avion, effectuer les contrôles essentiels à la surveillance du site et à la prévention des risques, vérifier le bon dimensionnement des bagages, vérifier le poids du bagage correspond aux normes exigées par la compagnie, protéger et alerter en cas d'accident ou d’événement exceptionnel"
     },
     {
         // title: "Certificat de Qualification Professionnelle Agent de Sûreté Aéroportuaire",
@@ -41,14 +53,16 @@ export const DataParcours = [
         ecole: "ICTS France Training",
         // contrat: "CDD",
         date: "2017",
-        logo: "CQP.png"
+        logo: "CQP.png",
+        description: "Certificat de Qualification Professionnelle Agent de Sûreté Aéroportuaire, Formation certificat SST niveau 1 (Sauveteur secouriste du travail)"
     },
     {
         title: "Agent Polyvalent Factotum",
         entreprise: "SNCF & Dexia Crédit Agricole",
         contrat: "CDD",
         date: "2016",
-        logo: "facilitess.png"
+        logo: "facilitess.png",
+        description: "Interventions diverses (déplacement de petit mobilier, réapprovisionnement des Consommables), gestion des archives, affranchissement/répartition du courrier. Courrier et gestion documentaire (réceptionner, trier, dispatcher), gestion de salles de réunion (agencement des tables et chaises, vérification du matériel audiovisuel), factotum (montage de meubles, fixation de tableaux, manutentions diverses)"
     },
     {
         title: "Licence Economie et Gestion",
@@ -61,19 +75,23 @@ export const DataParcours = [
         title: "Bac STMG Gestion Finance",
         ecole: "Lycée Paul Eluard",
         date: "2015",
-        logo: "paris8.png"
+        logo: "paris8.png",
+        description: "Baccalauréat Sciences et Technologies du Management et de la Gestion Spécialisation en Gestion Finance (mention assez bien)"
     },
     {
         title: "Assistant Administratif",
         entreprise: "GDF SUEZ",
         date: "2014",
-        logo: "gdf.png"
+        logo: "gdf.png",
+        description: "Réorganisations des archives et des salles d’archivages"
     },
     {
         title: "Assistant Informatique",
         entreprise: "SAGE",
+        contrat: "Stage",
         date: "2011",
-        logo: "sage.png"
+        logo: "sage.png",
+        description: "Identifier des ressources nécessaires à la résolution d'un dysfonctionnement, configurer le poste de travail aux besoins de l'utilisateur et effectuer les tests de fonctionnement, réaliser les opérations de nettoyage et de réglage, réaliser une configuration standard sur un poste de travail, configurer un serveur réseau, assister techniquement des collaborateurs."
     }
 ];
 
