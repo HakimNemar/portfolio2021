3cb7572c2320d203ac53b85a0d8efa71a636f59a
hakim-nemar <hakim.nemar@epitech.eu>
2022-11-01 15:16:07 +0100

    ajout function handleclick video et style

--- src/components/Pages/Projects/Realisations/test.tsx
+++ src/components/Pages/Projects/Realisations/test.tsx
@@ -1,12 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, SyntheticEvent } from 'react';
 import { DataRealisationsTest } from '../../../Common/data';
 
 export default function RealisationsTest() {
     const [data, setData] = useState(Array);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     useEffect(() => {
         setData(DataRealisationsTest);
     }, []);
+
+    const handleClick = (e: SyntheticEvent) => {
+        let element = e.target as HTMLElement;
+
+
+        if (element.tagName === "VIDEO") {
+            // if (isPlaying) {
+            //     setIsPlaying(false);
+            //     element.style.zIndex = "-2";
+            //     (element as HTMLVideoElement).pause();
+            // }
+
+            if ((element as HTMLVideoElement).paused) {
+
+            } else {
+                (element as HTMLVideoElement).pause();
+                element.style.zIndex = "-2";
+            }
+        } else {
+            // if (!isPlaying) {
+            //     setIsPlaying(true);
+            //     (element.children[0] as HTMLElement).style.zIndex = "1";
+            //     (element.children[0] as HTMLVideoElement).play();
+            // }
+
+            if ((element.children[0] as HTMLVideoElement).paused) {
+                (element.children[0] as HTMLVideoElement).play();
+                (element.children[0] as HTMLElement).style.zIndex = "1";
+            }
+        }
+    }
 
     return (
         <ul className="realisationsTest">
@@ -21,8 +53,8 @@ export default function RealisationsTest() {
                                 <p>{e.description}</p>
                                 <a href={e.url} target="_blank" rel="noreferrer">Jeter un coup d'oeil</a>
                             </div>
-                            <figure className="ProjectThumbnail_figure">
-                                <video loop muted >
+                            <figure className="ProjectThumbnail_figure" data-cursor-hover onClick={handleClick}>
+                                <video loop muted data-cursor-hover>
                                     <source type="video/mp4" src={require(`../../../../assets/video/${e.video}`).default} />
                                 </video>
                             </figure>
